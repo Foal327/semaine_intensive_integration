906b25510d8185a5dd6afcb849bd4b0c321fd87b
Alexandreslama <alexandre.slama@hetic.net>
2016-02-18 17:41:45 +0100

    Devis et LAB - Alexandre

---
+++ js/oopsColorChange.js
@@ -0,0 +1,52 @@
+/********** IMAGE **********/
+$("#caseCouleur1").click(function(){
+  $("#couleurOopsPerso").attr("src", "../images/prod2.png")
+});
+
+$("#caseCouleur2").click(function(){
+  $("#couleurOopsPerso").attr("src", "../images/prod1.png")
+});
+
+$("#caseCouleur3").click(function(){
+  $("#couleurOopsPerso").attr("src", "../images/prod3.png")
+});
+
+$("#caseCouleur4").click(function(){
+  $("#couleurOopsPerso").attr("src", "../images/prod4.png")
+});
+
+/********** LOGO **********/
+
+$("#logoEmplacement1").click(function(){
+  $("#logoOopsPerso").attr("src", "../images/logo1.png")
+});
+
+$("#logoEmplacement2").click(function(){
+  $("#logoOopsPerso").attr("src", "../images/logo2.png")
+});
+
+$("#logoEmplacement3").click(function(){
+  $("#logoOopsPerso").attr("src", "../images/logo3.png")
+});
+
+$("#pasDeLogoEmplacement").click(function(){
+  $("#logoOopsPerso").attr("src", " ")
+});
+
+/********** MOT **********/
+
+$("#motEmplacement1").click(function(){
+  $("#motOopsPerso").attr("src", "../images/Lorem1.png")
+});
+
+$("#motEmplacement2").click(function(){
+  $("#motOopsPerso").attr("src", "../images/Lorem2.png")
+});
+
+$("#motEmplacement3").click(function(){
+  $("#motOopsPerso").attr("src", "../images/Lorem3.png")
+});
+
+$("#pasDeMotEmplacement").click(function(){
+  $("#motOopsPerso").attr("src", " ")
+});
